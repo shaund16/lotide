16a894d888dc5ceaaed978beb0748f6c11616606
Shaun <s-purslow@hotmail.com>
2021-06-28 19:35:47 +0000

    add gitignore and organize head files

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,16 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-//TEST CODE
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//TEST CODE
+describe('#head', () => {
+
+  it('returns 5 for [5,6,7]', () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
